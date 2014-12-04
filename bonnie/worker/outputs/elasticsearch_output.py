0b35436653eb5394c684da906c9acba746afa83b
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2014-12-04 11:04:24 +0100

    Update license header

--- bonnie/worker/outputs/elasticsearch_output.py
+++ bonnie/worker/outputs/elasticsearch_output.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2010-2014 Kolab Systems AG (http://www.kolabsys.com)
+#
+# Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen a kolabsys.com>
+# Thomas Bruederli (Kolab Systems) <bruederli a kolabsys.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 import datetime
 
 from dateutil.parser import parse
@@ -58,6 +78,7 @@ class ElasticSearchOutput(object):
             }
 
         log = { '@version': bonnie.API_VERSION }
+
         for key,val in notification.iteritems():
             newkey = keymap[key] if keymap.has_key(key) else key
             if newkey is not None:
