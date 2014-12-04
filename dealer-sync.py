3efd5d38907c6025146618bb3becbc2b247955f6
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2014-12-04 11:02:29 +0100

    Provide a dealer-sync and dealer-async (forks)

---
+++ dealer-sync.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2010-2014 Kolab Systems AG (http://www.kolabsys.com)
+#
+# Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen a kolabsys.com>
+# Thomas Bruederli (Kolab Systems) <bruederli a kolabsys.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import json
+import os
+import sys
+
+from bonnie.dealer import BonnieDealer
+
+if __name__ == "__main__":
+    notification = sys.stdin.read().strip()
+
+    dealer = BonnieDealer()
+    dealer.run(notification)
+
+#    newpid = os.fork()
+#
+#    if newpid == 0:
+#        dealer = BonnieDealer()
+#        dealer.run(notification)
